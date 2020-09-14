52bcd6976e0a2dbc982f5332f05e59fd7f902ee9
Khalid <khalid@ayctor.com>
2020-09-14 15:48:30 +0200

    first commit

---
+++ src/svg-loader.vue
@@ -0,0 +1,30 @@
+<template>
+    <svg role="img"><use :xlink:href="src" /></svg>
+</template>
+
+<script>
+export default {
+    name: 'SVGLoader',
+    props: ['src', 'classList'],
+    mounted() {
+        this.setContent();
+    },
+    methods: {
+        setContent() {
+            if (this.classList && this.classList !== '') {
+                const classListArray = this.classList.split(' ');
+                if (classListArray && classListArray.length > 0) {
+                    classListArray.forEach(element => {
+                        this.$el.classList.add(element);
+                    });
+                }
+            }
+        },
+    },
+    watch: {
+        src() {
+            this.setContent();
+        },
+    },
+};
+</script>
